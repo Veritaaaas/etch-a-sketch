835ef117cd2cd4867c1a300516178a1e54511c0f
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2023-11-11 14:29:06 +0800

    provided an option to clear and resize the grid

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 //creates the sketchpad
-function createSketchpad()
+function createSketchpad(row = 16, col = 16)
 {
     const grid = document.querySelector('.sketchpad');
 
-    for (let row = 0; row < 16; row++)
+    grid.innerHTML = '';
+
+    grid.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+
+    for (let i = 0; i < row; i++)
     {
-        for (let col = 0; col < 16; col++)
+        for (let j = 0; j < col; j++)
         {
+            console.log(i, j)
             const cell = document.createElement('div');
             cell.classList.add('cell');
             grid.appendChild(cell);
@@ -27,5 +33,31 @@ function changeColor()
     }
 }
 
-window.onload = createSketchpad;
+function clear()
+{
+    const cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < cells.length; i++)
+    {
+        cells[i].style.backgroundColor = 'white';
+    }
+
+}
+
+window.onload = createSketchpad();
 let intervalId = setInterval(changeColor, 1000);
+
+const clearButton = document.querySelector('.clear');
+const sizeButton = document.querySelectorAll('.size');
+
+for (let i = 0; i < sizeButton.length; i++)
+{
+    sizeButton[i].addEventListener('click', function() 
+    {
+        let size = parseInt(sizeButton[i].value);
+        createSketchpad(size, size);
+    });
+}
+
+
+
+clearButton.addEventListener('click', clear);
